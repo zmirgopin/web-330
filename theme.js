c05b66e9d86d9af5dde7f6c3b2bd86faf7f22e43
zmirgopin <zahavagopin@gmail.com>
2023-01-15 15:25:40 -0500

    week-2

--- theme.js
+++ theme.js
@@ -1,3 +1,12 @@
+    /**
+================================
+Title: theme.js
+Author: Zahava Gopin 
+Date: 3 January 2023
+Description: JavaScript for WEB 330 
+================================
+*/
+
 /**
  * setting the default theme to light. 
  */
@@ -15,3 +24,47 @@ function setSelectedTheme()
     document.body.classList.value= localStorage.getItem("mode") || "light-theme";
 }
 
+        /**
+         * Setting the default theme to light 
+         */
+
+        setDefaultTheme();
+        /**
+         * This function switches the users selected website theme.
+         */
+        function toggleMode(x){
+            let colorTheme = document.body.classList; //get the body's CSS class
+            let iconMode = x.classList;//assigneign the triggered button to the current classes.
+        
+        /**
+         * If the current body class is set to the light-theme, update the users's preferance to the dark-theme in the browsers.
+         */
+            if (colorTheme.value==="light-theme")
+            {
+                localStorage.clear();
+                localStorage.setItem("mode", "dark-theme");
+                localStorage.setItem("iconMode", "fa-toggle-on");
+                localStorage.setItem("iconText", "Dark Mode");
+            }
+
+        /**
+         * If the current body class is set to the dark-theme, update the user preference to the light-theme in the browser
+         */
+            else
+            {
+                localStorage.clear();
+                localStorage.setItem("mode", "light-theme");
+                localStorage.setItem("iconMode", "fa-toggle-off");
+                localStorage.setItem("iconText", "Light Mode");
+            }
+
+        /**
+         * Apply the updated selection to the HRML page elements 
+         */
+            colorTheme.value = localStorage.getItem("mode");
+
+            iconMode.value =   `fa
+            ${localStorage.getItem("iconMode")} pull-right`;
+
+            document.getElementById("icon-text").innerHTML= localStorage.getItem("iconText");
+        }
