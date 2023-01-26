bfcf91d604eacfffd4cdc10647628ab7b67d4c13
zmirgopin <zahavagopin@gmail.com>
2023-01-25 20:11:21 -0500

    week 4

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -1,26 +1,34 @@
-"use strict"; 
-import {FoodModel} from "./food-model.js";
+/**
+================================
+Title: calorie-converter.js
+Author: Zahava Gopin 
+Date: 25 January 2023
+Description: JavaScript for calorie app
+================================
+*/
+/**
+ * Importing the FoodModel class to use with the new class called CalorieConverter.
+ * The new class includes all of the data for the FoodModel objects.
+ * Export the class. 
+ */
+"use strict";
+import { FoodModel } from "./food-model.js";
 
-export class CalorieConverter extends FoodModel
-{
- 
+export class CalorieConverter {
+
     static data = [
-        new FoodModel(1007, 'Egg', 78), 
-        new FoodModel(1008, 'Apple', 95), 
-        new FoodModel(1009, 'Hamburger', 354), 
-        new FoodModel(1010, 'Fries', 400), 
-        new FoodModel(1011, 'Banana', 105), 
+        new FoodModel(1007, 'Egg', 78),
+        new FoodModel(1008, 'Apple', 95),
+        new FoodModel(1009, 'Hamburger', 354),
+        new FoodModel(1010, 'Fries', 400),
+        new FoodModel(1011, 'Banana', 105),
         new FoodModel(1012, 'Soda', 150)
     ];
 
 
-
-    static find (str)
-    { 
-        data.filter(function(data)
-        {
-        return data.name  === str
-        });
+//Using the built in filter method to go through the data and assign it to a new array. 
+    static find(str) {
+        return this.data.filter(data => data.name.toLowerCase().includes(str.toLowerCase()))
     };
 };
 
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,3 +1,16 @@
+/**
+================================
+Title: food-model.js
+Author: Zahava Gopin 
+Date: 25 January 2023
+Description: JavaScript for calorie app
+================================
+*/
+/**
+ * Exporting a class called FoodModel.
+ * Constructors for the class include id, name, and calories. 
+ */
+"use strict";
 export class FoodModel {
     constructor (id, name, calories)
     {
